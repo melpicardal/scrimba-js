055fc2aac066d79cc006ab8e36c40cf34b1ce267
meljun picardal <meljun.one@gmail.com>
2021-09-17 11:32:26 +0900

    added if else logic to blackjack

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,13 @@
 let firstCard = 6
-let secondCard = 9
+let secondCard = 15
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+  console.log("Do you want to draw a new card? 🤔")
+}
+else if (sum === 21) {
+  console.log("You won! 🎉")
+}
+else {
+  console.log("You lost 😭")
+}
