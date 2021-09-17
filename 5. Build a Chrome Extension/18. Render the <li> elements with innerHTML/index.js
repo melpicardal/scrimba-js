ee449727bbedbd9d65f430b35d0e688efb1ed6f2
meljun picardal <meljun.one@gmail.com>
2021-09-17 21:59:27 +0900

    clear input field after save

--- 5. Build a Chrome Extension/18. Render the <li> elements with innerHTML/index.js
+++ 5. Build a Chrome Extension/18. Render the <li> elements with innerHTML/index.js
@@ -10,8 +10,5 @@ inputBtn.addEventListener("click", function() {
 
 // Replace .textContent with .innerHTML and use <li> tags
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
 }
-
-
-
--- 5. Build a Chrome Extension/22. Clear the input field/index.js
+++ 5. Build a Chrome Extension/22. Clear the input field/index.js
@@ -6,6 +6,7 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // Clear out the input field
+    inputEl.value = ""
     renderLeads()
 })
 
@@ -14,5 +15,5 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
         listItems += "<li>" + myLeads[i] + "</li>"
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
