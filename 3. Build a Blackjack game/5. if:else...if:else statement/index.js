bfc8f7e82251e5fc97b5a8d9481c302f0a0582ae
meljun picardal <meljun.one@gmail.com>
2021-09-17 11:41:21 +0900

    birthday card from the king of Norway at 100yrs

--- 3. Build a Blackjack game/5. if:else...if:else statement/index.js
+++ 3. Build a Blackjack game/5. if:else...if:else statement/index.js
@@ -3,5 +3,12 @@
 let age = 100
 
 // if less than 100    -> "Not elegible"
+if (age < 100) {
+  console.log("Not eligible")
+} else if (age === 100) {
+  console.log("Here is your birthday card from the King!")
+} else {
+  console.log("Not eligible, you've already gotten one")
+}
 // else if exactly 100 -> "Here is your birthday card from the King!"
 // else                -> "Not elegible, you have already gotten one"
