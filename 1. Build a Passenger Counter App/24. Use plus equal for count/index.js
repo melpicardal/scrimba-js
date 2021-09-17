702b103e1f7214f895922422e0d8004d9f886baa
meljun picardal <meljun.one@gmail.com>
2021-09-17 09:52:36 +0900

    completed #24

--- 1. Build a Passenger Counter App/24. Use plus equal for count/index.js
+++ 1. Build a Passenger Counter App/24. Use plus equal for count/index.js
@@ -3,12 +3,10 @@ let count = 0
 
 function increment() {
     // Change this to use the plus equal technique you've learned
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
 function save() {
     console.log(count)
 }
-
-
