5c05bdd7c27a520e85da7ea29193fd2bac83bdd7
meljun picardal <meljun.one@gmail.com>
2021-09-18 01:26:45 +0900

    getItem localStorage setup

--- 5. Build a Chrome Extension/36. Get the leads from localStorage/index.js
+++ 5. Build a Chrome Extension/36. Get the leads from localStorage/index.js
@@ -5,14 +5,17 @@ const ulEl = document.getElementById("ul-el")
 
 // Get the leads from the localStorage
 // Store it in a variable, leadsFromLocalStorage
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 // Log out the variable
+console.log(leadsFromLocalStorage)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
-    
+
     // To verify that it works:
     console.log( localStorage.getItem("myLeads") )
 })
@@ -28,5 +31,5 @@ function renderLeads() {
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
